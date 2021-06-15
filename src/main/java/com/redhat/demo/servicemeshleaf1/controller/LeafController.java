a34fff57b044ea445c93a2b81db974f0c60138d8
Jay Shollenberger <jshollen@redhat.com>
2021-06-15 16:53:56 -0600

    openshift service mesh objects, call counter

--- src/main/java/com/redhat/demo/servicemeshleaf1/controller/LeafController.java
+++ src/main/java/com/redhat/demo/servicemeshleaf1/controller/LeafController.java
@@ -5,20 +5,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class LeafController {
+
+    private static AtomicLong callCount = new AtomicLong(0);
 
     @Autowired
     private ClientService clientService;
 
     @GetMapping("/leaf1")
     public String leaf1() {
-        return "leaf1";
+        return new StringBuilder("leaf1")
+                .append(", call Count: " + callCount.incrementAndGet())
+                .toString();
     }
 
     @GetMapping("callleaf2")
     public String callLeaf2() {
         String response = clientService.callLeaf2();
+        StringBuilder finalResponse = new StringBuilder(response).append("\n")
+                .append("call count: " + callCount.incrementAndGet());
         return response;
     }
 }
