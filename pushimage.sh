4516926c2740d01aec1265ef1a376ae63aa55049
Jay Shollenberger <jshollen@redhat.com>
2021-06-17 16:52:59 -0600

    delay, this didn't work out.

--- pushimage.sh
+++ pushimage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh 
-docker tag servicemesh-leaf1:latest docker.io/sholly/servicemesh-leaf1:1.0
-docker push docker.io/sholly/servicemesh-leaf1:1.0
+docker tag servicemesh-leaf1:latest docker.io/sholly/servicemesh-leaf1:delay
+docker push docker.io/sholly/servicemesh-leaf1:delay
--- src/main/java/com/redhat/demo/servicemeshleaf1/controller/LeafController.java
+++ src/main/java/com/redhat/demo/servicemeshleaf1/controller/LeafController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -16,8 +17,13 @@ public class LeafController {
     private ClientService clientService;
 
     @GetMapping("/leaf1")
-    public String leaf1() {
+    public String leaf1() throws InterruptedException {
+        Random rand = new Random();
         callCount.incrementAndGet();
+        int sleep = rand.nextInt();
+        if(sleep > 0)
+            Thread.sleep(2000);
+
         System.out.println("leaf1 v10, count: " + callCount.get());
         return new StringBuilder("leaf1 version1.0 : ")
                 .append(callCount.get())
